25187c7c97fba170ee7f113bae21376f7fbf01aa
hurinhu <hurin@live.ca>
2020-05-18 11:58:14 +1200

    2.1.2 Add code to fix compile errors for testCreateCourse()

--- src/main/java/Course.java
+++ src/main/java/Course.java
@@ -1,3 +1,5 @@
+import java.util.*;
+
 public class Course {
 	private Database db;
 	private int id;
@@ -5,6 +7,10 @@ public class Course {
     public Course(){
 
     }
+
+	public Course(Database db) {
+		this.db = db;
+	}
 
 	public Course(Database db, int course_id) {
 		this.db = db;
@@ -21,5 +27,9 @@ public class Course {
 		} else {
 			throw new RuntimeException("Course is not exist");
 		}
+	}
+
+	public boolean create(String description,int capacity,boolean concession,List<String> compulsory_program,List<String> prerequisite,List<String> timeslots,String location,String status) {
+		return true;
 	}
 }
