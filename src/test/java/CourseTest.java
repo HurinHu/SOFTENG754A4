3106a423dae22ac3e3e8a9810db2563cc087e792
NoMacAlive <37617906+NoMacAlive@users.noreply.github.com>
2020-05-18 15:19:32 +1200

    11.2.3 fixed one error in test and the test passed

--- src/test/java/CourseTest.java
+++ src/test/java/CourseTest.java
@@ -148,6 +148,7 @@ public class CourseTest {
   public void testGetCourseLocation() {
     int course_id = 123;
     Database db = Mockito.mock(Database.class);
+    Mockito.when(db.getCourseLocation(course_id)).thenReturn("201N-346");
     Course course = new Course(db, course_id);
     String location = course.getLocation();
     assertEquals("201N-346",location);
