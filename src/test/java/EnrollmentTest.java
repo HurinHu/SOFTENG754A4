55e4ad2c772e66f046a99888eb4045ec35793aa9
hurinhu <hurin@live.ca>
2020-05-18 12:27:27 +1200

    1.3.1 Add failing testGetRemainingSeats() test case

--- src/test/java/EnrollmentTest.java
+++ src/test/java/EnrollmentTest.java
@@ -38,4 +38,39 @@ public class EnrollmentTest {
     }
   }
 
+  @Test
+  public void testGetRemainingSeats() {
+    Course course = Mockito.mock(Course.class);
+    Database db = Mockito.mock(Database.class);
+    // class full
+    Mockito.when(course.getCourseId()).thenReturn(123);
+    Mockito.when(db.getCapacity(123)).thenReturn(10);
+    Mockito.when(db.getEnrolled(123)).thenReturn(10);
+    Enrollment enrollment = new Enrollment(db);
+    int remaining = enrollment.getRemainingSeats(course);
+    assertEquals(0, remaining);
+    // remain 10 seats
+    Mockito.when(course.getCourseId()).thenReturn(125);
+    Mockito.when(db.getCapacity(125)).thenReturn(20);
+    Mockito.when(db.getEnrolled(125)).thenReturn(10);
+    remaining = enrollment.getRemainingSeats(course);
+    assertEquals(10, remaining);
+    // course id not exist
+    try{
+      Mockito.when(course.getCourseId()).thenReturn(-1);
+      remaining = enrollment.getRemainingSeats(course);
+    } catch (Exception e) {
+      assertEquals("Course is not exist", e.getMessage());
+    }
+    // remaining seats less than zero
+    try{
+      Mockito.when(course.getCourseId()).thenReturn(126);
+      Mockito.when(db.getCapacity(126)).thenReturn(10);
+      Mockito.when(db.getEnrolled(126)).thenReturn(20);
+      remaining = enrollment.getRemainingSeats(course);
+    } catch (Exception e) {
+      assertEquals("Internal Error", e.getMessage());
+    }
+  }
+
 }
