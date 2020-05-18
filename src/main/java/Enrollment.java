492e3e70a55bc1318c11ed423d4f3a5b8b89530e
hurinhu <hurin@live.ca>
2020-05-18 12:19:08 +1200

    1.1.3 Add code to make testGetCourseCapacity() pass

--- src/main/java/Enrollment.java
+++ src/main/java/Enrollment.java
@@ -3,10 +3,18 @@ public class Enrollment {
   private Database db;
 
   public Enrollment(Database db) {
+	  this.db = db;
   }
 
   public int getCourseCapacity(Course course) {
+    int course_id = -1;
     int capacity = -1;
+    course_id = course.getCourseId();
+    if (course_id != -1) {
+      capacity = this.db.getCapacity(course_id);
+    } else {
+      throw new RuntimeException("Course is not exist");
+    }
     return capacity;
   }
 
