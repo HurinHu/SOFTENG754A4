2ccb9d3516741952a9af793e83d4097c3d5999dd
NoMacAlive <37617906+NoMacAlive@users.noreply.github.com>
2020-05-18 16:18:32 +1200

    added test for checking time clash for one hour rule

--- src/test/java/UserTest.java
+++ src/test/java/UserTest.java
@@ -19,4 +19,27 @@ public class UserTest {
         String courseTime = user.getCourseTime(course_id);
         assertEquals("Monday 14:00-15:00",courseTime);
     }
+
+    @Test
+    public void testCheckTimeClashOneHour(){
+        int user_id = 123;
+        int user2_id = 345;
+        Database db = Mockito.mock(Database.class);
+        User user = new User(db,user_id);
+        Hashtable<Integer, String> table = new Hashtable();
+        table.put(123,"Monday 14:00-15:00");
+        table.put(123,"Monday 14:00-15:00");
+        table.put(345,"Monday 14:00-15:00");
+        table.put(345,"Monday 14:00-15:00");
+        Mockito.when(db.getEnrolledCourse(user_id)).thenReturn(table);
+        boolean timeClashAllowed = user.checkTimeTableClashOneHour();
+        assertEquals(timeClashAllowed,true);
+
+        User user2 = new User(db,user2_id);
+        Hashtable<Integer, String> Gpatable = new Hashtable();
+        table.put(123,"9");
+        table.put(345,"6");
+        Mockito.when(db.getUsersGpa(user2_id)).thenReturn(Gpatable);
+        assertEquals(timeClashAllowed,false);
+    }
 }
