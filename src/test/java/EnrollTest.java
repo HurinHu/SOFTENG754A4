8dc56ba76c5dd80418181495dc49528e027bf0db
Jiaqiu Zhao <jzha653@aucklanduni.ac.nz>
2020-05-21 00:53:01 +1200

    Integration Test: add MeetRequirementAndEnrollTest

--- src/test/java/EnrollTest.java
+++ src/test/java/EnrollTest.java
@@ -1,6 +1,23 @@
+import static org.junit.Assert.assertEquals;
+
+import org.junit.Test;
 import org.junit.experimental.categories.Category;
+import org.mockito.*;
 
 @Category(IntegrationTest.class)
 public class EnrollTest {
-
+	  @Test
+	  public void testMeetRequirementAndEnroll() {
+		  Database db = Mockito.mock(Database.class);
+		  Course course = new Course(db);
+		  User user = Mockito.mock(User.class);
+		  Enrollment enrol = new Enrollment(db,user);
+		  Mockito.when(db.enrollCourse(user.getId(), course.getCourseId())).thenReturn(true);
+		  Mockito.when(db.checkEligibility(course, user)).thenReturn(true);
+		  boolean result = false;
+		  if(course.checkEligibility(course, user) == true) {
+			  result = enrol.enrollCourse(course);
+		  }
+		  assertEquals(result,true);
+	  }
 }
