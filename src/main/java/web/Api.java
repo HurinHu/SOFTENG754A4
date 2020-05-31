71835197e657f71929940e3a55311fe1cc9869b2
XiaoDangJia <zhuguangya98@gmail.com>
2020-05-31 21:16:00 +1200

    cross origin

--- src/main/java/web/Api.java
+++ src/main/java/web/Api.java
@@ -3,12 +3,11 @@ package web;
 import java.util.*;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
-import org.springframework.web.bind.annotation.RequestMethod;
-import org.springframework.web.bind.annotation.RequestParam;
+
+import org.springframework.web.bind.annotation.*;
 import org.springframework.web.servlet.ModelAndView;
 
+@CrossOrigin
 @RestController("/")
 public class Api {
 	Database db = new Database();
