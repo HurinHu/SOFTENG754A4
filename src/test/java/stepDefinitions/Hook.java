99a3d4da5f6347d68e20300311131c2864fb0f39
hurinhu <hurin@live.ca>
2020-06-04 13:26:25 +1200

    fixed selenium quit issue

--- src/test/java/stepDefinitions/Hook.java
+++ src/test/java/stepDefinitions/Hook.java
@@ -1,6 +1,7 @@
 import base.BaseUtil;
 
 import java.util.*;
+import java.util.logging.Level;
 import java.util.concurrent.TimeUnit;
 import org.apache.commons.io.FileUtils;
 import java.io.File;
@@ -9,6 +10,7 @@ import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.chrome.ChromeOptions;
 import org.openqa.selenium.TakesScreenshot;
 
+import java.io.*;
 import io.cucumber.java.After;
 import io.cucumber.java.Before;
 import io.cucumber.java.Scenario;
@@ -28,6 +30,9 @@ public class Hook  extends BaseUtil {
 		ChromeOptions options = new ChromeOptions();
         options.addArguments("headless");
         options.addArguments("window-size=1200x600");
+        System.setProperty("webdriver.chrome.args", "--disable-logging");
+        System.setProperty("webdriver.chrome.silentOutput", "true");
+        java.util.logging.Logger.getLogger("org.openqa.selenium").setLevel(Level.OFF);
 		if (isWindows()) {
 			System.setProperty("webdriver.chrome.driver", "driver/chromedriver.exe");
 			this.base.driver = new ChromeDriver(options);
@@ -42,15 +47,55 @@ public class Hook  extends BaseUtil {
 	}
 
 	@After
-	public void closeBrowser(Scenario scenario){
-		this.takeSnapShot("target/screenshot/"+this.base.screenshot) ;
+	public void closeBrowser(Scenario scenario) {
+		this.takeSnapShot("target/screenshot/"+this.base.screenshot);
+		this.base.driver.close();
 		this.base.driver.quit();
-        if (scenario.isFailed()) {
+		if (scenario.isFailed()) {
             System.out.println("Scenario Failed: "+scenario.getName());
         } else {
             System.out.println("Scenario Passed: "+scenario.getName());
         }
+		if (isWindows()) {
+			cleanCommand("taskkill /F /IM chromedriver.exe /T");
+		} else if (isMac()) {
+			cleanCommand("ps aux | grep chromedriver | awk '{print $2}' | xargs kill -9 $1");
+		} else if (isUnix()) {
+			cleanCommand("ps aux | grep /opt/google/chrome/chrome | awk '{print $2}' | xargs kill -9 $1");
+			cleanCommand("ps aux | grep chromedriver | awk '{print $2}' | xargs kill -9 $1");
+		}
+	}
 
+	public void cleanCommand(String cmd) {
+		ProcessBuilder processBuilder = new ProcessBuilder();
+		processBuilder.command("bash", "-c", cmd);
+		try {
+
+			Process process = processBuilder.start();
+
+			StringBuilder output = new StringBuilder();
+
+			BufferedReader reader = new BufferedReader(
+					new InputStreamReader(process.getInputStream()));
+
+			String line;
+			while ((line = reader.readLine()) != null) {
+				output.append(line + "\n");
+			}
+
+			int exitVal = process.waitFor();
+			if (exitVal == 0) {
+				System.out.println("Success!");
+				System.out.println(output);
+				System.exit(0);
+			} else {
+			}
+
+		} catch (IOException e) {
+			e.printStackTrace();
+		} catch (InterruptedException e) {
+			e.printStackTrace();
+		}
 	}
 
 	public boolean isWindows() {
